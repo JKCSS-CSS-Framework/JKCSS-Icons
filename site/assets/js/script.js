8842ff0001440036ad5c6764c725fd8331f06261
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-14 21:43:39 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- site/assets/js/script.js
+++ site/assets/js/script.js
@@ -4,9 +4,17 @@ fetch('navbar.html')
 .then(data=>{
     nav.innerHTML=data
 });
+
 const footer = document.querySelector('.footerbar')
 fetch('footerbar.html')
 .then(res=>res.text())
 .then(data=>{
     footer.innerHTML=data
+});
+
+const get_nav = document.querySelector('.get-nav')
+fetch('get_nav.html')
+.then(res=>res.text())
+.then(data=>{
+    get_nav.innerHTML=data
 });
